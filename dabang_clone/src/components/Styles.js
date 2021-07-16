3c2f8bc011e7e783819d4b3ad765ea623a2a5b34
kyungjoon.park <pkj6921@naver.com>
2021-07-16 22:44:34 +0900

    header view 만들기

--- dabang_clone/src/components/Styles.js
+++ dabang_clone/src/components/Styles.js
@@ -5,62 +5,62 @@ const Grid = styled.div`
   box-sizing: border-box;
   display: flex;
   flex-direction: ${(props) =>
-    props.flex_direction === "column" ? "column" : "row"};
+    props.flexDirection === "column" ? "column" : "row"};
   ${(props) => (props.border ? `border: ${props.border};` : "")}
-  ${(props) => (props.bg ? `background-color: ${props.bg};` : "")}
+  ${(props) =>
+    props.backgroundColor ? `background-color: ${props.backgroundColor};` : ""}
   width: ${(props) => (props.width ? props.width : "100%")};
   min-width: 50px;
   height: ${(props) => (props.height ? props.height : "100%")};
-  align-items: ${(props) => (props.align ? props.align : "center")};
+  align-items: ${(props) => (props.alignItems ? props.alignItems : "center")};
   justify-content: ${(props) =>
-    props.justify_contents ? props.justify_contents : "flex-start"};
+    props.justifyContent ? props.justifyContent : "flex-start"};
   ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
   ${(props) => (props.padding ? `padding: ${props.padding};` : "")}
   ${(props) => (props.is_root ? `width: 100vw; height: 100vh;` : "")}
   ${(props) =>
     props.hover ? `&:hover{cursor: pointer; background-color: #44444455;}` : ""}
-    ${(props) => (props.color ? `color:${props.color};` : "")}
+  ${(props) => (props.color ? `color:${props.color};` : "")}
   ${(props) => (props.textAlign ? `text-align: center;` : "")}
+  border-bottom: ${(props) => props.borderBottom || ""};
+  flex: ${(props) => props.flex || ""};
+  font-family: ${(props) => props.fontFamily || ""};
 `;
 
 const Text = (props) => {
   if (props.type === "title") {
-    return <H1>{props.children}</H1>;
+    return <H1 {...props}>{props.children}</H1>;
   }
-
-  if (props.type === "contents") {
-    return <P>{props.children}</P>;
-  }
-
   if (props.type === "label") {
-    return <Span>{props.children}</Span>;
+    return <Span {...props}>{props.children}</Span>;
   }
-
-  return <React.Fragment>{props.children}</React.Fragment>;
+  return <P {...props}>{props.children}</P>;
 };
-
 const H1 = styled.h1`
   margin: 0px;
   font-size: 1.5em;
   text-align: center;
   ${(props) => (props.bold ? `font-weight: bold;` : "")}
 `;
-
 const P = styled.p`
-  ${(props) => (props.fontsize ? `font-size: ${props.fontsize};` : "font-size: 1em" )}
-  ${(props) => (props.fontweight ? `font-weight: ${props.fontweight};` : "")}
+  ${(props) =>
+    props.fontSize ? `font-size: ${props.fontSize};` : "font-size: 1em"}
+  ${(props) => (props.fontWeight ? `font-weight: ${props.fontWeight};` : "")}
   ${(props) => (props.color ? `color: ${props.color};` : "")}
   ${(props) => (props.margin ? `margin: ${props.margin};` : "")}
-  ${(props) => (props.textalign ? `text-align: ${props.textalign};` : "")}
+  ${(props) => (props.textAlign ? `text-align: ${props.textAlign};` : "")}
+  line-height: ${(props) => props.lineHeight || ""};
+  &:hover {
+    color: ${(props) => props.hoverColor || ""};
+    background-color: ${(props) => props.hoverBackgroundColor || ""};
+  }
+  cursor: ${(props) => props.cursor || ""};
 `;
-
 const Span = styled.span`
   margin: 0px;
   font-size: 0.4em;
   color: #888;
-  font-family: "PoppinsR";
 `;
-
 
 // input 스타일!
 // const InputC = styled.input`
